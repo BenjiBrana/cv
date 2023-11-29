2e2ea18e82a42325d8e369d37d44a0c443fe587d
Benji Brana <benjidebran@gmail.com>
2023-11-29 09:54:52 +0100

    1er depot

---
+++ index.php
@@ -0,0 +1,178 @@
+<?php
+$title = "BRANA BENJAMIN | Curriculum vitae";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?= $title ?></title>
+    <link rel="stylesheet" href="Design/css/default.css">
+</head>
+
+<body>
+    <section>
+        <article id="article1">
+            <img id="imgProfil" src="./Assets/img/photo_logo.webp" alt="Photo de profil" title="Photo de profil">
+            <h2>Présentation</h2>
+            <div id="presentation">
+                <p>
+                    <span>&#127968;</span>
+                    15 rue du Chanvre <br> 16507 - Saint Genis d'Hiersac</p>
+                
+                <p><span>&#128241;</span> 06.42.94.70.59</p>
+                
+                <p><span>&#128231;</span> benjidebran@gmail.com</p>
+                
+                <p><span>&#127874;</span> 31 ans</p>
+                
+                <p><span>&#128664;</span> Permis B</p>
+            </div>
+            <h2>Portfolio, Github</h2>
+            <div id="reseaux">
+                <p>
+                    <img src="./Assets/img/logo/logo_linkedin.png" alt="Linkedin" title="Linkedin">
+                    https://www.linkedin.com/in/benjamin-brana-002610179/
+                </p>
+                <p>
+                <img class="imgStyle" src="./Assets/img/logo/logo_web.png" alt="Portfolio" title="Portfolio">
+                    https://benjibrana.github.io/portfolio/
+                </p>
+                <p>
+                    <img class="imgStyle" src="./Assets/img/logo/logo_web.png" alt="Github" title="Github">
+                    https://github.com/BenjiBrana
+                </p>
+            </div>
+            <hr>
+            <div id="profil">
+                <h2>Profil professionnel</h2>
+                <p>Actuellement au chômage, j'utilise mon temps à bonne escient en me formant en autodidacte pour le métier de développeur web. <br/><br/>
+                Je recherche une formation diplômante qui me permettra de consolider mes bases et m'apporter les connaissances manquantes pour être compétent dans le métier de développeur web.<br/><br/>
+                Volontaire, je me distingue par mon engagement au service du collectif en m'intègrant sans difficultés et par mon goût d'apprendre.<br/><br/>
+                </p>
+            </div>
+        </article>
+
+        <article id="article2">
+            <header>
+                <h1>BRANA BENJAMIN
+                    <small># Curriculum vitae</small>
+                </h1>
+                <h2>Développeur web junior</h2>
+            </header>
+            <div class="card">
+                <h2><img src="./Assets/img/logo/logo_competence.jpg" alt="Logo compétences" title="Logo compétences"><b>compétences</b></h2>
+                <ul class="ulCard">
+                    <li>Base en développement wweb (HTML, CSS, BOOTSTRAP, TAILWIND, SASS).</li>
+                    <li>Apprentissage en cours de JAVASCRIPT, PHP, REACT.</li>
+                    <li>Création de site web.</li>
+                    <li>Maquettage avec Figma.</li>
+                    <li>Maîtrise d'internet, CHAT-GPT, WORD, EXCEL, informatique.</li>
+                    <li>Tâches en rapport avec la logistique(manutention, réception, expédition, mise en stock).</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h2><img src="./Assets/img/logo/logo_experiences.jpg" alt="Logo compétences" title="Logo compétences"><b>expériences</b></h2>
+                <ul class="ulCard">
+                    <li>
+                        <p class="pTitle">
+                            <b>Stage d'immersion développeur web</b> Freelanceweb16 - Mouthiers-sur-Boëme 09/2023<br/>
+                        </p>
+                        <ul>
+                            <li>Mise en situation du métier de développeur web.</li>
+                            <li>Découverte du Framework React.</li>
+                            <li>Codage avec les langages de programmation web (HTML, CSS, JAVASCRIPT, PHP).</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="pTitle">
+                            <b>Manutentionnaire, livreur, magasinier</b> SPA Destribats - Dax 08/2013 - 03/2023<br/>
+                        </p>
+                        <ul>
+                            <li>Préparation des produits en vue de leur transport et de leur expédition en sélectionnant l'emballage et le contenant appropriés.</li>
+                            <li>Réception des marchandises et vérification de leur conformité par rapport aux bons de commande et aux bons de livraison.</li>
+                            <li>Préparation des commandes à l'aide de checklists.</li>
+                            <li>Réception des marchandises, vérification des quantités et de la qualité conformément aux commandes.</li>
+                            <li>Déplacement des marchandises au sein de l'entrepôt à l'aide de chariots élévateurs et de transpalettes électriques.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="pTitle">
+                            <b>Apprenti</b> SPA Destribats - Dax 08/2011 - 07/2013<br/>
+                        </p>
+                        <ul>
+                            <li>Apprentissage du métier de magasinier.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="pTitle">
+                            <b>Agent Polyvalent</b> Office public de l'habitat - Dax 08/2009<br/>
+                        </p>
+                        <ul>
+                            <li>Travaux de maintenance et petites réparations sur les installations et les équipements.</li>
+                            <li>Tonte des pelouses avec la tondeuse.</li>
+                            <li>Entretien ménager des locaux avec nettoyage des sols et des vitres, vidage des poubelles.</li>
+                            <li>Ramassage des détritus sur la voie publique.</li>
+                            <li>Déplacement des marchandises au sein de l'entrepôt à l'aide de chariots élévateurs et de transpalettes électriques.</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <h2><img src="./Assets/img/logo/logo_formation.jpg" alt="Logo compétences" title="Logo compétences"><b>Formation</b></h2>
+                <ul class="ulCard">
+                    <li>
+                        <p class="pTitle">
+                            <b>CAP Magasinier</b> CFA - Mont-de-Marsan 08/2009 - 07/2011<br/>
+                        </p>
+                    </li>
+                    <li>
+                        <p class="pTitle">
+                            <b>CAP Logistique</b> Lycée Haroun Tazieff - Saint-Paul-Lès-Dax 09/2007 - 06/2009<br/>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div class="card">
+                <h2><img src="./Assets/img/logo/logo_formation.jpg" alt="Logo compétences" title="Logo compétences"><b>Centres d'intérêt</b></h2>
+                <ul class="ulCard">
+                    <li>Passion pour l'informatique et la Tech</li>
+                    <li>Passion pour le Football</li>
+                </ul>
+            </div>
+            <div class="card">
+                <h2><img src="./Assets/img/logo/logo_formation.jpg" alt="Logo compétences" title="Logo compétences"><b>Langues</b></h2>
+                <ul class="ulCard">
+                    <li>
+                        <p class="pTitle">
+                            <b>Français</b> Langue maternelle<br/>
+                        </p>
+                    </li>
+                    <li>
+                        <p class="pTitle">
+                            <b>Anglais</b> Débutant<br/>
+                        </p>
+                    </li>
+                    <li>
+                        <p class="pTitle">
+                            <b>Espagnol</b> Débutant<br/>
+                        </p>
+                    </li>
+                    
+                </ul>
+            </div>
+            
+
+        </article>
+        <footer>
+            <p>Curriculum vitae codé en site web par :</p>
+            <p>&copy; Brana Benjamin |  <?php echo date('Y') ?> </p>
+        </footer>
+    </section>
+    
+
+</body>
+
+</html>
